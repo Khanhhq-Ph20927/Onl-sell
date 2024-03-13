6908c40500343b3009a750d4318f04964bea89e2
hieucv25 <chuhieu05@gmail.com>
2024-03-14 00:03:30 +0700

    filter by color and size + sort

--- TonerTestApp/src/main/java/com/project/SportsStores/Toner/Controller/client/SanPhamRestController.java
+++ TonerTestApp/src/main/java/com/project/SportsStores/Toner/Controller/client/SanPhamRestController.java
@@ -29,61 +29,72 @@ public class SanPhamRestController {
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
-//    @RequestMapping(value = "/page/{pageNumber}/{keyWord}", method = RequestMethod.GET)
-//    private ResponseEntity<?> pagination(@PathVariable("pageNumber") int pageNumber,
-//                                         @PathVariable("keyWord") String keyWord) {
-//        Pageable pageable = PageRequest.of(pageNumber, 8, Sort.by("ngayTao").descending());
-//        if (keyWord.equalsIgnoreCase("null")) {
-//            Page<SanPham> page = service.pageClient(pageable);
-//            return new ResponseEntity<>(page, HttpStatus.OK);
-//        } else {
-//            Page<SanPham>   page = service.searchAndPrice( pageable,keyWord,priceStart,priceEnd);
-//            return new ResponseEntity<>(page, HttpStatus.OK);
-//        }
-//    }
-
     @RequestMapping(value = "/page/{pageNumber}/{keyWord}", method = RequestMethod.POST)
     private ResponseEntity<?> paginationAndFilter(@PathVariable("pageNumber") String pageNumber,
                                                   @PathVariable("keyWord") String keyWord,
-                                                  @RequestBody FilterRequest filterRequest) {
+                                                  @RequestBody FilterRequest filterRequest,
+                                                  @RequestParam("sort") int sort) {
         Pageable pageable = PageRequest.of(Integer.parseInt(pageNumber), 8, Sort.by("ngayTao").descending());
-//        Page<SanPham> page = service.pageClient(pageable);
         Page<SanPham> page = null;
-        if(filterRequest.getListColors() != null) {
-            for (Integer result : filterRequest.getListColors()
-            ) {
-                System.out.println("result :" + result);
-            }
+        if (sort == 1) {
+            Sort sortCriteria = Sort.by(
+                    Sort.Order.asc("tenSP"),
+                    Sort.Order.desc("ngayTao")
+            );
+            pageable = PageRequest.of(Integer.parseInt(pageNumber), 8, sortCriteria);
+            System.out.println("sort A-Z");
+        } else if (sort == 2) {
+            Sort sortCriteria = Sort.by(
+                    Sort.Order.desc("tenSP"),
+                    Sort.Order.desc("ngayTao")
+            );
+            pageable = PageRequest.of(Integer.parseInt(pageNumber), 8, sortCriteria);
+            System.out.println("sort Z-A");
+        } else if (sort == 3) {
+            Sort sortCriteria = Sort.by(
+                    Sort.Order.asc("donGia"),
+                    Sort.Order.desc("ngayTao")
+
+            );
+            pageable = PageRequest.of(Integer.parseInt(pageNumber), 8, sortCriteria);
+            System.out.println("sort price desc");
+        } else if (sort == 4) {
+            Sort sortCriteria = Sort.by(
+                    Sort.Order.desc("donGia"),
+                    Sort.Order.desc("ngayTao")
+            );
+            pageable = PageRequest.of(Integer.parseInt(pageNumber), 8, sortCriteria);
+            System.out.println("sort price asc");
+        } else {
+            System.out.println("sort default");
         }
+        if (keyWord.equals("null") && (filterRequest.getListColors() == null && filterRequest.getListSizes() == null)) {
+            page = service.pageClient(pageable);
+            System.out.println("Case Default");
 
-        if(filterRequest.getListSizes() != null) {
-            for (String result : filterRequest.getListSizes()
-            ) {
-                System.out.println("result :" + result);
-            }
-        }
-        //2 done, 1 done , 3 done, 4 done , 5 done
-
-        //search
-
-        if (!keyWord.equals("null") && (filterRequest.getListColors() == null && filterRequest.getListSizes() == null)) {
+        } else if (!keyWord.equals("null") && (filterRequest.getListColors() == null && filterRequest.getListSizes() == null)) {
             page = service.search(pageable, keyWord);
             System.out.println(keyWord);
             System.out.println("Case " + 1);
 
         } else if (keyWord.equals("null") && filterRequest.getListColors() != null && filterRequest.getListSizes() == null) {
+
             page = service.filterColor(pageable, filterRequest.getListColors());
             System.out.println("Case " + 2);
-        }
-        else if (keyWord.equals("null") && filterRequest.getListColors() == null && filterRequest.getListSizes() != null) {
+        } else if (keyWord.equals("null") && filterRequest.getListColors() == null && filterRequest.getListSizes() != null) {
+
             page = service.filterSize(pageable, filterRequest.getListSizes());
             System.out.println("Case " + 3);
         } else if (keyWord.equals("null") && filterRequest.getListColors() != null && filterRequest.getListSizes() != null) {
+
             page = service.filterColorAndSize(pageable, filterRequest.getListColors(), filterRequest.getListSizes());
             System.out.println("Case " + 4);
         } else if (!keyWord.equals("null") && filterRequest.getListColors() != null && filterRequest.getListSizes() != null) {
+
             page = service.searchfilterColorAndSizeIn(pageable, keyWord, filterRequest.getListColors(), filterRequest.getListSizes());
             System.out.println("Case " + 5);
+        } else {
+            System.out.println("Error??");
         }
         return new ResponseEntity<>(page, HttpStatus.OK);
     }
